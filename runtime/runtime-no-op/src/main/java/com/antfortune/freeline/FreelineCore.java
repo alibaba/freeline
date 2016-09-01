43d6975e41aee706e8f1dbfe0b81d8e4819ed5da
Yong <lomanyong@gmail.com>
2016-09-01 20:28:52 +0800

    + add runtime-no-op

---
+++ runtime/runtime-no-op/src/main/java/com/antfortune/freeline/FreelineCore.java
@@ -0,0 +1,14 @@
+package com.antfortune.freeline;
+
+import android.app.Application;
+
+/**
+ * Created by huangyong on 16/9/1.
+ */
+public class FreelineCore {
+
+    public static void init(Application app) {
+        // nothing
+    }
+
+}
