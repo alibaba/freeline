18a81dd7cb390d671ab45b7ebc2896c5dc035ded
Yong <lomanyong@gmail.com>
2016-09-02 08:31:01 +0800

    * fix confict between apk path config and default apk finder

--- gradle/src/main/groovy/com/antfortune/freeline/FreelinePlugin.groovy
+++ gradle/src/main/groovy/com/antfortune/freeline/FreelinePlugin.groovy
@@ -38,6 +38,7 @@ class FreelinePlugin implements Plugin<Project> {
                 def extension = project.extensions.findByName("freeline") as FreelineExtension
                 def hack = extension.hack
                 def productFlavor = extension.productFlavor
+                def apkPath = extension.apkPath
                 def excludeHackClasses = extension.excludeHackClasses
                 def forceLowerVersion = extension.foceLowerVersion
 
@@ -55,19 +56,22 @@ class FreelinePlugin implements Plugin<Project> {
                     return
                 }
 
-                def apk_paths = []
-                variant.outputs.each { output ->
-                    def path = output.outputFile.absolutePath
-                    if (path.endsWith(".apk")) {
-                        apk_paths.add(output.outputFile.absolutePath)
+                // find the default apk
+                if (FreelineUtils.isEmpty(apkPath)) {
+                    def apk_paths = []
+                    variant.outputs.each { output ->
+                        def path = output.outputFile.absolutePath
+                        if (path.endsWith(".apk")) {
+                            apk_paths.add(output.outputFile.absolutePath)
+                        }
                     }
-                }
-                def descriptionFile = new File(FreelineUtils.joinPath(FreelineUtils.getFreelineCacheDir(project.rootDir.absolutePath), 'project_description.json'))
-                if (descriptionFile.exists()) {
-                    def description = new JsonSlurper().parseText(descriptionFile.text)
-                    description.apk_path = FreelineUtils.getDefaultApkPath(apk_paths, project.buildDir.absolutePath, project.name, description.product_flavor)
-                    println "find default apk path: ${description.apk_path}"
-                    FreelineUtils.saveJson(new JsonBuilder(description).toPrettyString(), descriptionFile.absolutePath, true)
+                    def descriptionFile = new File(FreelineUtils.joinPath(FreelineUtils.getFreelineCacheDir(project.rootDir.absolutePath), 'project_description.json'))
+                    if (descriptionFile.exists()) {
+                        def description = new JsonSlurper().parseText(descriptionFile.text)
+                        description.apk_path = FreelineUtils.getDefaultApkPath(apk_paths, project.buildDir.absolutePath, project.name, description.product_flavor)
+                        println "find default apk path: ${description.apk_path}"
+                        FreelineUtils.saveJson(new JsonBuilder(description).toPrettyString(), descriptionFile.absolutePath, true)
+                    }
                 }
 
                 // force tasks to run
