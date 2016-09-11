8dafe11b29c33d64895e5c7f3d4d2d6b2463f6da
act262 <act262@gmail.com>
2016-09-11 18:39:33 +0800

    add freeline run configuration for idea.

---
+++ android-studio-plugin/src/com/antfortune/freeline/plugin/configuration/FreeConfigurationFactory.java
@@ -0,0 +1,25 @@
+package com.antfortune.freeline.plugin.configuration;
+
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Freeline Configuration Factory
+ *
+ * @author act262@gmail.com
+ */
+class FreeConfigurationFactory extends ConfigurationFactory {
+
+    FreeConfigurationFactory(@NotNull ConfigurationType type) {
+        super(type);
+    }
+
+    @NotNull
+    @Override
+    public RunConfiguration createTemplateConfiguration(@NotNull Project project) {
+        return new FreeRunConfiguration(project, this, "FreelineFactory....");
+    }
+}
