6ade6a89d2b92a19a72679a0867a44d0cc6d6f0c
act262 <act262@gmail.com>
2016-09-11 21:45:29 +0800

    check the run configuration conditions

--- android-studio-plugin/src/com/antfortune/freeline/plugin/utils/SystemUtil.java
+++ android-studio-plugin/src/com/antfortune/freeline/plugin/utils/SystemUtil.java
@@ -1,12 +1,25 @@
 package com.antfortune.freeline.plugin.utils;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.SystemInfo;
+
+import java.io.File;
+
 /**
- * Created by micro on 2016/9/6 0006.
+ * System Utility Wrapper
+ *
+ * @author act262@gmail.com
  */
 public class SystemUtil {
+
+    /**
+     * os system is windows or other
+     */
     public static boolean isWindows() {
-        String os = System.getenv("os");
-        return os.equalsIgnoreCase("Windows_NT");
+//        String os = System.getenv("os");
+//        return os.equalsIgnoreCase("Windows_NT");
+
+        return SystemInfo.isWindows;
     }
 
     public static boolean hasPython() {
@@ -15,5 +28,18 @@ public class SystemUtil {
 
     public static boolean hasAndroidSDK() {
         return !System.getenv("ANDROID_HOME").isEmpty();
+    }
+
+    /**
+     * 当前项目是否初始化过了Freeline
+     */
+    public static boolean hasInitFreeline(Project project) {
+        if (project != null) {
+            File file = new File(project.getBasePath(), "freeline.py");
+            if (file.exists()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
