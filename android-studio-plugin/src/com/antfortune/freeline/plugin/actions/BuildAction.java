ca53a263146a0b23f8f8b60d703c69160172edba
act262 <act262@gmail.com>
2016-09-12 01:59:25 +0800

    build action setup with basic console output

--- android-studio-plugin/src/com/antfortune/freeline/plugin/actions/BuildAction.java
+++ android-studio-plugin/src/com/antfortune/freeline/plugin/actions/BuildAction.java
@@ -1,16 +1,23 @@
 package com.antfortune.freeline.plugin.actions;
 
-import com.antfortune.freeline.plugin.runnable.BuildRunnable;
 import com.antfortune.freeline.plugin.utils.SystemUtil;
-import com.antfortune.freeline.plugin.utils.Util;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
+import com.intellij.execution.process.OSProcessHandler;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.ProcessTerminatedListener;
+import com.intellij.execution.ui.ConsoleView;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
-
-import java.io.File;
-import java.util.ArrayList;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.impl.ContentImpl;
 
 /**
  * build or init freeline action
@@ -19,54 +26,71 @@ import java.util.ArrayList;
  */
 public class BuildAction extends AnAction {
 
-    public static void run(File dir, AnActionEvent e) {
-        File file = new File(dir, "freeline.py");
-        // 已经初始化过了，直接执行
-        if (file.exists()) {
-            // python freeline.py
-            new Thread(new BuildRunnable(dir, file, e)).start();
-        } else {
-            // 未初始化，先下载下来
-            initFreeline(dir);
-        }
-    }
-
-    public static void initFreeline(File dir) {
-        ArrayList<String> cmds = new ArrayList<>();
-        // for windows
-        if (SystemUtil.isWindows()) {
-            // gradlew.bat initFreeline -Pmirror
-            cmds.add("gradlew.bat");
-            cmds.add("initFreeline");
-            cmds.add("-Pmirror");
-        } else {
-            // for *unix
-            // ./gradlew initFreeline -Pmirror
-            cmds.add("./gradlew");
-            cmds.add("initFreeline");
-            cmds.add("-Pmirror");
-        }
-        Util.execCmd(cmds, dir, true);
-    }
+    private final static String TOOL_ID = "Freeline Console";
 
     @Override
     public void actionPerformed(AnActionEvent e) {
         Project currentProject = DataKeys.PROJECT.getData(e.getDataContext());
         FileDocumentManager.getInstance().saveAllDocuments();
 
-        // TODO: 16-9-5  Android SDK环境检测、Python环境检测
-//        if (!SystemUtil.hasPython()) {
-//            System.out.println("Not found python");
-//            return;
-//        }
-//        if (!SystemUtil.hasAndroidSDK()) {
-//            System.out.println("Not found Android_HOME");
-//            return;
-//        }
+        GeneralCommandLine commandLine = new GeneralCommandLine();
+        commandLine.setWorkDirectory(currentProject.getBasePath());
 
-        File dir = new File(currentProject.getBasePath());
+        // execute freeline script
+        if (SystemUtil.hasInitFreeline(currentProject)) {
+            commandLine.setExePath("python");
+            commandLine.addParameters("freeline.py");
+        } else {
+            // init freeline core file
+            if (SystemUtil.isWindows()) {
+                commandLine.setExePath("cmd");
+                commandLine.addParameter("gradlew.bat");
+            } else {
+                commandLine.setExePath("/bin/sh");
+                commandLine.addParameter("./gradlew");
+            }
+            commandLine.addParameters("initFreeline", "-Pmirror");
+        }
 
-        run(dir, e);
+        // TODO: 2016/9/12 0012 重复操作、超时处理
+
+        // commands process
+        processCommandline(currentProject, commandLine);
+    }
+
+    /* process command line */
+    private void processCommandline(Project project, GeneralCommandLine commandLine) {
+        try {
+            OSProcessHandler processHandler = new OSProcessHandler(commandLine);
+            ProcessTerminatedListener.attach(processHandler);
+            processHandler.startNotify();
+//            processHandler.waitFor();
+
+            processConsole(project, processHandler);
+        } catch (ExecutionException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    /* process attach to console,show the log */
+    private void processConsole(Project project, ProcessHandler processHandler) {
+        // TODO: 2016/9/12 0012 窗口调整
+        ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
+        ToolWindow toolWindow;
+        toolWindow = toolWindowManager.getToolWindow(TOOL_ID);
+        if (toolWindow != null) {
+            toolWindow.show(null);
+            return;
+        }
+
+        ConsoleView consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
+        consoleView.attachToProcess(processHandler);
+        toolWindow = toolWindowManager.registerToolWindow(TOOL_ID, true, ToolWindowAnchor.BOTTOM);
+        toolWindow.setTitle("free....");
+        toolWindow.setShowStripeButton(true);
+        toolWindow.setIcon(IconLoader.getIcon("/icons/ic_free.jpg"));
+        toolWindow.getContentManager().addContent(new ContentImpl(consoleView.getComponent(), "", true));
+        toolWindow.show(null);
     }
 
     private void showTip() {
