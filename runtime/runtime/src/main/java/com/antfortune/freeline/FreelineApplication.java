fa01fc31d88bb8b3207bd27129f05b675f7d4941
Yong <lomanyong@gmail.com>
2016-09-18 18:30:05 +0800

    + support Application incremental build

---
+++ runtime/runtime/src/main/java/com/antfortune/freeline/FreelineApplication.java
@@ -0,0 +1,85 @@
+package com.antfortune.freeline;
+
+import android.app.Application;
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.antfortune.freeline.util.ReflectUtil;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Created by huangyong on 16/9/14.
+ */
+
+public class FreelineApplication extends Application {
+
+    protected static final String TAG = "FreelineApplication";
+
+    private Class freelineConfigClazz;
+
+    private Application realApplication;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "FreelineApplication#onCreate()");
+        FreelineCore.init(this);
+        initFreelineConfig();
+        createRealApplication();
+        startRealApplication();
+    }
+
+    private void startRealApplication() {
+        if (realApplication != null) {
+            try {
+                ReflectUtil.invokeMethod(Application.class, realApplication, "attach", new Class[]{Context.class}, new Object[]{getBaseContext()});
+                Log.d(TAG, "realApplication#attach(Context)");
+            } catch (Exception e) {
+                FreelineCore.printStackTrace(e);
+                Log.e(TAG, "attach with realApplication error");
+            }
+
+            realApplication.onCreate();
+            Log.d(TAG, "realApplication#onCreate()");
+        }
+    }
+
+    private void initFreelineConfig() {
+        try {
+            freelineConfigClazz = Class.forName("com.antfortune.freeline.FreelineConfig");
+        } catch (Exception e) {
+            FreelineCore.printStackTrace(e);
+            Log.e(TAG, "initFreelineConfig error");
+        }
+    }
+
+    private String getConfigValue(String fieldName) {
+        try {
+            return ReflectUtil.getStaticFieldValue(freelineConfigClazz, fieldName).toString();
+        } catch (Exception e) {
+            FreelineCore.printStackTrace(e);
+            Log.e(TAG, "get config value error");
+            return "";
+        }
+    }
+
+    private void createRealApplication() {
+        String applicationClass = getConfigValue("applicationClass");
+        if (TextUtils.isEmpty(applicationClass)) {
+            realApplication = new Application();
+            Log.d(TAG, "create empty application.");
+        } else {
+            try {
+                Class realClass = Class.forName(applicationClass);
+                Constructor<? extends Application> constructor = realClass.getConstructor();
+                this.realApplication = constructor.newInstance();
+                Log.d(TAG, "create application: " + applicationClass);
+            } catch (Exception e) {
+                FreelineCore.printStackTrace(e);
+                Log.e(TAG, "create real application error");
+            }
+        }
+    }
+}
