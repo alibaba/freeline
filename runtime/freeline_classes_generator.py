2fc2d7ded1ce2641f1dec94e1bca2e9cd707bba8
Yong <lomanyong@gmail.com>
2016-09-28 13:59:03 +0800

    * force freeline runtime classes in main dex

--- runtime/freeline_classes_generator.py
+++ runtime/freeline_classes_generator.py
@@ -9,17 +9,14 @@ def main():
     print('      freeline code generator       ')
     print('====================================')
 
-    target_dir = os.path.join('runtime', 'src', 'main', 'java')
+    target_dir = os.path.join('runtime', 'build', 'intermediates', 'classes', 'release')
     freeline_classes = []
     for dirpath, dirnames, files in os.walk(target_dir):
         for fn in files:
-            if fn.endswith('.java'):
-                class_name = os.path.join(dirpath, fn.replace('.java', '.class')).replace(target_dir + '/', '')
+            if fn.endswith('.class'):
+                class_name = os.path.join(dirpath, fn).replace(target_dir + '/', '')
                 freeline_classes.append(class_name)
-                if fn == 'FreelineService.java':
-                    freeline_classes.append(class_name.replace('.class', '$InnerService.class'))
 
-    freeline_classes.append('com/antfortune/freeline/BuildConfig.class')
     freeline_classes.append('com/antfortune/freeline/FreelineConfig.class')
 
     for clazz in freeline_classes:
