d90455b75cc94599be7c3925d69489bd41dbd905
Yong <lomanyong@gmail.com>
2016-10-21 16:15:32 +0800

    + add log when app run with runtime-no-op

--- runtime/runtime-no-op/src/main/java/com/antfortune/freeline/FreelineCore.java
+++ runtime/runtime-no-op/src/main/java/com/antfortune/freeline/FreelineCore.java
@@ -1,14 +1,25 @@
 package com.antfortune.freeline;
 
 import android.app.Application;
+import android.util.Log;
 
 /**
  * Created by huangyong on 16/9/1.
  */
 public class FreelineCore {
 
+    private static final String TAG = "Freeline";
+
+    public static void init(Application app, Application realApplication) {
+        markFreeline();
+    }
+
     public static void init(Application app) {
-        // nothing
+        markFreeline();
+    }
+
+    private static void markFreeline() {
+        Log.i(TAG, "Freeline with runtime-no-op loaded!");
     }
 
 }
