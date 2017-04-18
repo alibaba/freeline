e74b0b3a6d280e925b0a843952216ebd296ed9f9
hejie3 <hejie3@ucweb.com>
2017-04-18 15:46:51 +0800

    增加打包时判断手机是否切换,如果手机切换,就先将本地包安装再更新增量包,避免每次都需要重新打包

--- freeline_core/android_tools.py
+++ freeline_core/android_tools.py
@@ -169,6 +169,8 @@ class UpdateStatTask(Task):
     def execute(self):
         cache_path = os.path.join(self._config['build_cache_dir'], 'stat_cache.json')
         stat_cache = load_json_cache(cache_path)
+        cache_path_md5 = os.path.join(self._config['build_cache_dir'], 'stat_cache_md5.json')
+        stat_cache_md5 = load_json_cache(cache_path_md5)
 
         for module, file_dict in self._changed_files.iteritems():
             for key, files in file_dict.iteritems():
@@ -180,10 +182,14 @@ class UpdateStatTask(Task):
                             if fpath not in stat_cache[module]:
                                 stat_cache[module][fpath] = {}
 
+                            if fpath in stat_cache_md5:
+                                stat_cache_md5[fpath] = get_md5(fpath)
+
                             stat_cache[module][fpath]['mtime'] = os.path.getmtime(fpath)
-                            stat_cache[module][fpath]['md5'] = get_md5(fpath)
+                            stat_cache[module][fpath]['size'] = os.path.getsize(fpath)
 
         write_json_cache(cache_path, stat_cache)
+        write_json_cache(cache_path_md5, stat_cache_md5)
 
 
 class DirectoryFinder(object):
